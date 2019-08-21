8ff2ac0385e23f7e9d9244f7e3ca6c6eb795d4e0
MichaelSam <michaelsam08@gmail.com>
2019-08-21 12:43:33 -0500

    Updates

---
+++ precache-manifest.0bd9428ba08b84e8ef8cd6d67355e809.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2d3f40caa3db5734a9744bc6a23c1b",
+    "url": "/michaelsam08/AboutMePortal/index.html"
+  },
+  {
+    "revision": "7b91bbaeab9ea2199e1a",
+    "url": "/michaelsam08/AboutMePortal/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "1580ea29af9f0d1dacca",
+    "url": "/michaelsam08/AboutMePortal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7b91bbaeab9ea2199e1a",
+    "url": "/michaelsam08/AboutMePortal/static/js/2.14a9720f.chunk.js"
+  },
+  {
+    "revision": "1580ea29af9f0d1dacca",
+    "url": "/michaelsam08/AboutMePortal/static/js/main.d4a0cac2.chunk.js"
+  },
+  {
+    "revision": "9ee40b5a3982efbe5962",
+    "url": "/michaelsam08/AboutMePortal/static/js/runtime~main.5562ba8f.js"
+  },
+  {
+    "revision": "714fa24bdb6c59767ded8bc76252a08a",
+    "url": "/michaelsam08/AboutMePortal/static/media/Peru.714fa24b.jpg"
+  },
+  {
+    "revision": "5a7a0b5a06530444e70689b50c0c42bb",
+    "url": "/michaelsam08/AboutMePortal/static/media/cartagena1.5a7a0b5a.jpg"
+  },
+  {
+    "revision": "02c548ebab8881199b391b370b8508f6",
+    "url": "/michaelsam08/AboutMePortal/static/media/kimsFinca.02c548eb.jpg"
+  },
+  {
+    "revision": "5590f57d0082118c8f8d330ede8ee27a",
+    "url": "/michaelsam08/AboutMePortal/static/media/myBike.5590f57d.jpg"
+  },
+  {
+    "revision": "3ac23b173136b8425137e7e4fe3700d5",
+    "url": "/michaelsam08/AboutMePortal/static/media/picasso.3ac23b17.jpg"
+  }
+]);
